2e8e4ebc1a17a4a4d7827da685510443a3ce20a4
Kauã Duarte <kaua.duarte@gfxconsultoria.com>
2023-10-18 15:25:02 -0300

    Seeding service

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,11 +1,15 @@
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<SalesWebMvcContext>(options => options.UseMySql(builder.Configuration.GetConnectionString("SalesWebMvcContext"),ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("SalesWebMvcContext"))));
 
-// string mySQLConnection = builder.Configuration.GetConnectionString("SalesWebMvcContext");
-// builder.Services.AddDbContext<SalesWebMvcContext>(options => options.UseMySql(mySQLConnection, ServerVersion.AutoDetect(mySQLConnection)));
+builder.Services.AddScoped<SeedingService>();
+//builder.Services.AddScoped<SellerService>();
+//builder.Services.AddScoped<DepartmentService>();
+//builder.Services.AddScoped<SalesRecordService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -16,9 +20,22 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    app.Services.CreateScope().ServiceProvider.GetRequiredService<SeedingService>().Seed();
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+var enUS = new CultureInfo("en-US");
+var localizationOptions = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture(enUS),
+    SupportedCultures = new List<CultureInfo> { enUS },
+    SupportedUICultures = new List<CultureInfo> { enUS }
+};
+app.UseRequestLocalization(localizationOptions);
+
+// Seeding Database
+app.Services.CreateScope().ServiceProvider.GetRequiredService<SeedingService>().Seed();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
